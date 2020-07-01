5d504dc4897989165c20c0558dcac2ee77fe6492
alexgorunescu97 <gorunescualexandru@yahoo.com>
2020-07-01 23:33:04 +0300

    Changing response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('It is working!');
 })
 
 app.post('/signin', (req, res) => {
@@ -48,7 +48,7 @@ app.post('/imageurl', (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
 
